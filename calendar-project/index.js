929e7847bfa85cb96c50070b5f0db73a8eb453cb
Shawn Urbach <surbach77@gmail.com>
2017-01-13 23:52:28 -0600

    Dates now cycle forward in time

--- calendar-project/index.js
+++ calendar-project/index.js
@@ -1,6 +1,6 @@
 var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August',
 'September', 'October', 'November', 'December']
-var currentIndex = 0;
+var currentIndex = 1;
 var currentMonth = months[currentIndex];
 var currentYear = 2017;
 
@@ -8,28 +8,17 @@ var cssChanger = function(num){
 	document.querySelector("ul:nth-child(3) > li:nth-child("+num+")").style.backgroundColor = "gray";
 }
 
-document.getElementById('future').addEventListener("click", function(){
-	currentIndex+=1;
-	if (currentIndex == 12){
-		currentIndex = 0;
-		currentYear +=1;
-	}
-	currentMonth = months[currentIndex];
-	document.getElementById("current").innerHTML = 
-	"&nbsp;&nbsp;"
-	+ currentMonth
-	+ "&nbsp;&nbsp;"
-	+ currentYear
-});
 
 
 
 
 var ViewModel = function() {
 	
-	
-//get days from month
 
+
+
+//------------------------------------------------------------SETUP
+//get days from month date object;
 this.getDaysInMonth = function(month, year) {
 	var date = new Date(year, month, 1);
 	var days = [];
@@ -42,80 +31,140 @@ this.getDaysInMonth = function(month, year) {
 
 //raw output of days   
 this.themonth = [];
-this.month = 0;
+this.month = 1;
 this.year = 2017;
 
-//filtered output of days
-this.themonth2 = [];
 
 
-this.themonth = getDaysInMonth(month, 2017);
 
-for (var i=0; i < themonth.length; i++){
-	themonth2.push(themonth[i].getDate());
-}
+//----------------------------------------------------STARTMONTH
 
-currMonth = themonth2.slice(0);
-alert(currMonth); //1-31
 
-var nextMonth = [];
-this.themonth = getDaysInMonth(month + 1, 2017);
+var currFiltered = ko.observableArray([29,30,31,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,
+16,17,18,19,20,21,22,23,24,25,26,27,28,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]);
 
-for (var i=0; i < themonth.length; i++){
-	nextMonth.push(themonth[i].getDate());
-}
-alert(nextMonth); //1-28
 
-var nextslice = nextMonth.slice(0,(42-themonth2.length));
-alert(nextslice); //1-11
-alert(nextslice.length); //11
-alert(currMonth.length); //31
-
-for (var i=0; i < nextslice.length; i++){
-			themonth2.push(nextslice[i]);
-		}
-alert(themonth2); //1-31 + 1-11
-
-var findtheindex = themonth2.lastIndexOf(1);
-alert(findtheindex); //31
-
-var previousSlice = themonth2.splice((findtheindex-(findtheindex-28)),(findtheindex-28));
-alert(previousSlice);
+console.log(currFiltered());
 
 
 
 
-		
-    this.dates1 = ko.observableArray(themonth2);
+
+	
+    this.dates1 = ko.observableArray(currFiltered());
 	this.days1 = ko.observableArray(['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']);
+	
 	this.shiftDays = function() {
+		
+	currentIndex+=1;
+	if (currentIndex == 12){
+		currentIndex = 0;
+		currentYear +=1;
+	}
+	currentMonth = months[currentIndex];
+	document.getElementById("current").innerHTML = 
+	"&nbsp;&nbsp;"
+	+ currentMonth
+	+ "&nbsp;&nbsp;"
+	+ currentYear
+		
+		
+		
+		
+		
 		month+=1;
 		if (month == 12) {
 			month = 0;
 			year +=1;
 		}
 		
-		this.themonth = getDaysInMonth(month, year);
-		this.dates1.removeAll();
-		for (var i=0; i < themonth.length; i++){
-			themonth2.push(themonth[i].getDate());
-		}
+		
+//--------------------------------------------------CURRENTMONTH(RAW)
+var currMonth = [];
+this.themonth = getDaysInMonth(month, year);
 
-		this.dates1.push(themonth2);
+for (var i=0; i < themonth.length; i++){
+	currMonth.push(themonth[i].getDate());
+}
+console.log(currMonth); //1-28
+
+
+//-------------------------------------------------PREVIOUSMONTH(RAW)
+var previousMonth = [];
+this.themonth = getDaysInMonth(month-1, year);
+
+for (var i=0; i < themonth.length; i++){
+	previousMonth.push(themonth[i].getDate());
+}
+console.log(previousMonth); //1-31
+
+
+//-----------------------------------------------------NEXTMONTH(RAW)
+var nextMonth = [];
+this.themonth = getDaysInMonth(month, year);
+
+for (var i=0; i < themonth.length; i++){
+	nextMonth.push(themonth[i].getDate());
+}
+console.log(nextMonth); //1-31
+
+//----------------------------------------------------FIND PREVIOUSMONTHSLICE
+
+var findtheindex = currFiltered().lastIndexOf(1);
+console.log(findtheindex); //31
+
+var previousMonthCopy = currFiltered().slice(0);
+var previousSlice = previousMonthCopy.splice((findtheindex-(findtheindex-28)),(findtheindex-28));
+console.log(previousSlice); //26-28
+
+
+//------------------------------------------------------------NEXTMONTH(FILTERED)
+var nextMonthFiltered = previousSlice.concat(nextMonth);
+console.log(nextMonthFiltered); //26-28 + 1-31
+
+
+//--------------------------------------------------------------FIND NEXTMONTHSLICE
+var listnums = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21];
+var nextslice = listnums.slice(0,(49-nextMonthFiltered.length));
+console.log(nextslice); //1-8
+
+
+//------------------------------------------------------------NEXTMONTH(FILTERED)
+nextMonthFiltered = nextMonthFiltered.concat(nextslice);
+console.log(nextMonthFiltered); //26-28 + 1-31 + 1-8
+
+var nextcurrFiltered = nextMonthFiltered.slice(0);
+
 		
 		
-		this.dates1.pop();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		this.dates1.removeAll();
+		
+		for (var i=0; i < 49; i++){
+			this.dates1.push(nextMonthFiltered[i]);
+		}
+		
+		currFiltered = ko.observableArray(nextMonthFiltered);
+		
 		
 	};
 			
 };
 
 
-
-
-
-
 ko.applyBindings(ViewModel);
+
+document.getElementById('future').addEventListener("click", function(){
+});
 
 
 
