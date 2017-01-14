3774e177c86f4d304ed4bdbaece304660014a11a
Shawn Urbach <surbach77@gmail.com>
2017-01-14 16:28:08 -0600

    Dates now cycle backward and forward in time

--- calendar-project/index.js
+++ calendar-project/index.js
@@ -9,14 +9,8 @@ var cssChanger = function(num){
 }
 
 
-
-
-
 var ViewModel = function() {
 	
-
-
-
 //------------------------------------------------------------SETUP
 //get days from month date object;
 this.getDaysInMonth = function(month, year) {
@@ -35,23 +29,39 @@ this.month = 1;
 this.year = 2017;
 
 
-
-
 //----------------------------------------------------STARTMONTH
 
+var startingDate1 = 19;
+var startingDate2 = 30;
 
-var currFiltered = ko.observableArray([29,30,31,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,
-16,17,18,19,20,21,22,23,24,25,26,27,28,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]);
+var date1 = new Date(2017,2,(startingDate1));
+var date2 = new Date(2017,0,(startingDate2));
+var day;
+var between = [date1];
+
+while(date2 <= date1) {
+    day = date1.getDate()
+    date1 = new Date(date1.setDate(--day));  
+    between.push(date1);
+}
+
+var DateList = [];
+
+for (var i=0; i < between.length; i++){
+	DateList.push(between[i].getDate());
+}
+DateList.reverse();
+DateList.shift();
 
 
-console.log(currFiltered());
 
 
+//--------------------------------------------------------------------------------
 
 
 
 	
-    this.dates1 = ko.observableArray(currFiltered());
+    this.dates1 = ko.observableArray(DateList);
 	this.days1 = ko.observableArray(['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']);
 	
 	this.shiftDays = function() {
@@ -67,101 +77,131 @@ console.log(currFiltered());
 	+ currentMonth
 	+ "&nbsp;&nbsp;"
 	+ currentYear
-		
-		
-		
-		
-		
+	
 		month+=1;
 		if (month == 12) {
 			month = 0;
 			year +=1;
 		}
 		
-		
-//--------------------------------------------------CURRENTMONTH(RAW)
-var currMonth = [];
-this.themonth = getDaysInMonth(month, year);
-
-for (var i=0; i < themonth.length; i++){
-	currMonth.push(themonth[i].getDate());
-}
-console.log(currMonth); //1-28
-
-
-//-------------------------------------------------PREVIOUSMONTH(RAW)
-var previousMonth = [];
-this.themonth = getDaysInMonth(month-1, year);
-
-for (var i=0; i < themonth.length; i++){
-	previousMonth.push(themonth[i].getDate());
-}
-console.log(previousMonth); //1-31
-
-
-//-----------------------------------------------------NEXTMONTH(RAW)
-var nextMonth = [];
-this.themonth = getDaysInMonth(month, year);
-
-for (var i=0; i < themonth.length; i++){
-	nextMonth.push(themonth[i].getDate());
-}
-console.log(nextMonth); //1-31
-
-//----------------------------------------------------FIND PREVIOUSMONTHSLICE
-
-var findtheindex = currFiltered().lastIndexOf(1);
-console.log(findtheindex); //31
-
-var previousMonthCopy = currFiltered().slice(0);
-var previousSlice = previousMonthCopy.splice((findtheindex-(findtheindex-28)),(findtheindex-28));
-console.log(previousSlice); //26-28
-
-
-//------------------------------------------------------------NEXTMONTH(FILTERED)
-var nextMonthFiltered = previousSlice.concat(nextMonth);
-console.log(nextMonthFiltered); //26-28 + 1-31
-
-
-//--------------------------------------------------------------FIND NEXTMONTHSLICE
-var listnums = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21];
-var nextslice = listnums.slice(0,(49-nextMonthFiltered.length));
-console.log(nextslice); //1-8
-
-
-//------------------------------------------------------------NEXTMONTH(FILTERED)
-nextMonthFiltered = nextMonthFiltered.concat(nextslice);
-console.log(nextMonthFiltered); //26-28 + 1-31 + 1-8
-
-var nextcurrFiltered = nextMonthFiltered.slice(0);
 
 		
+//-----------------------------------------------------------CLEANUP		
+			
+		startingDate1 += 28;
+		startingDate2 += 28;
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		this.dates1.removeAll();
-		
-		for (var i=0; i < 49; i++){
-			this.dates1.push(nextMonthFiltered[i]);
+		if (dates1().indexOf(1) > 5){
+			startingDate1 +=7;
+			startingDate2 +=7;	
 		}
 		
-		currFiltered = ko.observableArray(nextMonthFiltered);
+		var date1 = new Date(2017,2,(startingDate1));
+var date2 = new Date(2017,0,(startingDate2));
+var day;
+var between = [date1];
+
+while(date2 <= date1) {
+    day = date1.getDate()
+    date1 = new Date(date1.setDate(--day));  
+    between.push(date1);
+}
+
+var DateList = [];
+
+for (var i=0; i < between.length; i++){
+	DateList.push(between[i].getDate());
+}
+DateList.reverse();
+DateList.shift();
+		
+		
+	
+		
+	this.dates1.removeAll();
+		
+		for (var i=0; i < 49; i++){
+			this.dates1.push(DateList[i]);
+		}
 		
 		
 	};
+	
+
+//----------------------------------------------------------GO BACKWARDS
+	this.shiftDaysBack = function() {
+		currentIndex-=1;
+	if (currentIndex == -1){
+		currentIndex = 11;
+		currentYear -=1;
+	}
+	currentMonth = months[currentIndex];
+	document.getElementById("current").innerHTML = 
+	"&nbsp;&nbsp;"
+	+ currentMonth
+	+ "&nbsp;&nbsp;"
+	+ currentYear
+	
+		month-=1;
+		if (month == -1) {
+			month = 11;
+			year -=1;
+		}
+		
+//-----------------------------------------------------------CLEANUP		
 			
+		startingDate1 -= 28;
+		startingDate2 -= 28;
+		
+		if (dates1().indexOf(1) < 7){
+			startingDate1 -=7;
+			startingDate2 -=7;	
+		}
+		
+		var date1 = new Date(2017,2,(startingDate1));
+var date2 = new Date(2017,0,(startingDate2));
+var day;
+var between = [date1];
+
+while(date2 <= date1) {
+    day = date1.getDate()
+    date1 = new Date(date1.setDate(--day));  
+    between.push(date1);
+}
+
+var DateList = [];
+
+for (var i=0; i < between.length; i++){
+	DateList.push(between[i].getDate());
+}
+DateList.reverse();
+DateList.shift();
+		
+		
+	
+		
+	this.dates1.removeAll();
+		
+		for (var i=0; i < 49; i++){
+			this.dates1.push(DateList[i]);
+		}
+		
+		
+	};
+	
+	
+	
+	
 };
 
-
 ko.applyBindings(ViewModel);
+
+
+
+
+
+
+
 
 document.getElementById('future').addEventListener("click", function(){
 });
