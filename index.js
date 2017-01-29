ccef8a61534f8e02cc32f4d4396fe22fc99945fd
Shawn Urbach <surbach77@gmail.com>
2017-01-29 01:20:35 -0600

    Reorganized code structure

--- index.js
+++ index.js
@@ -8,95 +8,81 @@ window.onload = loadJanuary;
 
 var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug',
 'Sep', 'Oct', 'Nov', 'Dec']
-var currentIndex = 1;
+var currentMonthIndex = 1;
 var currentYear = 2017;
 var currentTimeIndex = 24205;
-var currentMonth = months[currentIndex];
-var DateList = [];
+var currentMonth = months[currentMonthIndex];
+var dateList = [];
 var pre_post_Dates = [];
-var date1;
-var date2;
-var day;
+var monthEnd;
+var monthStart;
 var between;
-
-var offset;
 var dateSelection;
 var selectIndex = 0;
 
-
-
 //------------------------------------------------------------------------------FUNCTIONS
 
-var cYear = function(){
+var returnUserYear = function(){
 	return this.userYear();
 }
 
-var cDate = function(){
+var returnUserDate = function(){
 	return this.userDate();
 }
 
-var dateLength = function(){
-	return this.userTest().length;	
+var eventLength = function(){
+	return this.eventList().length;	
 }
 
-//Selects list item and changes CSS style
-var cssChanger = function(num){
+//Changes CSS style if date not in current month
+var cssNotCurrentMonth = function(num){
 	document.querySelector("ul:nth-child(3) > li:nth-child("+num+")").style.backgroundColor = "gray";
 }
 
-var cssChanger2 = function(num){
+//Changes CSS style if date is selected
+var cssSelectedDate = function(num){
 	document.querySelector("ul:nth-child(3) > li:nth-child("+num+")").style.outlineColor = "red";
 	document.querySelector("ul:nth-child(3) > li:nth-child("+num+")").style.outlineWidth = "2px";
 	document.querySelector("ul:nth-child(3) > li:nth-child("+num+")").style.outlineStyle = "solid";
 }
 
-
-var colorChanger = function(){
-	
-		for (var i=0; i < userIndexes().length; i++){
-			if ((userIndexes()[i]) == currentTimeIndex){
-				document.getElementById("testit"+i).style.color = userColorArray()[i];
-		}
-	}
-	
+//Changes CSS style if date is an event day
+var cssEvents = function(num){
+	document.querySelector("ul:nth-child(3) > li:nth-child("+num+")").innerHTML = dateSelection + '<span id = "eventDot'+selectIndex+'">&#149;</span>';
+	selectIndex+=1;
 }
 
-var cssChanger3 = function(num){
-	
-
-			document.querySelector("ul:nth-child(3) > li:nth-child("+num+")").innerHTML = dateSelection + '<span id = "testit'+selectIndex+'">&#149;</span>';
-			selectIndex+=1;
-}
-
-var cssChanger4 = function(num){
-	
-
+//Removes event dot
+var cssDefault = function(num){
 	document.querySelector("ul:nth-child(3) > li:nth-child("+num+")").innerHTML = dateSelection;
-
-	
 }
 
-var cssChanger5 = function(num){
-	
-
+//Removes CSS changes
+var cssRemove = function(num){
 	document.querySelector("ul:nth-child(3) > li:nth-child("+num+")").style.outlineColor = "white";
 	document.querySelector("ul:nth-child(3) > li:nth-child("+num+")").style.outlineWidth = "0px";
 	document.querySelector("ul:nth-child(3) > li:nth-child("+num+")").style.outlineStyle = "solid";
-
-	
 }
 
+//Changes colors of event dots
+var colorChanger = function(){
+	for (var i=0; i < userIndexes().length; i++){
+		if ((userIndexes()[i]) == currentTimeIndex){
+			document.getElementById("eventDot"+i).style.color = userColorArray()[i];
+		}
+	}
+}
 
 //Creates array of prepended and postpended days for style change
 var createCSSChangeArray = function(){
-	for (var i=0; i < DateList.length; i++){
-		if (DateList[i] > 14 && DateList.indexOf(DateList[i]) < 14) {
-			pre_post_Dates.push(DateList.indexOf(DateList[i]));
+	for (var i=0; i < dateList.length; i++){
+		if (dateList[i] > 14 && dateList.indexOf(dateList[i]) < 14) {
+			pre_post_Dates.push(dateList.indexOf(dateList[i]));
 		}
 	}
-	for (var i=0; i < DateList.length; i++){
-		if (DateList[i] < 20 && DateList.lastIndexOf(DateList[i]) > 30){
-			pre_post_Dates.push(DateList.lastIndexOf(DateList[i]));
+	for (var i=0; i < dateList.length; i++){
+		if (dateList[i] < 20 && dateList.lastIndexOf(dateList[i]) > 30){
+			pre_post_Dates.push(dateList.lastIndexOf(dateList[i]));
 		}
 	}
 	for (var i=0; i < pre_post_Dates.length; i++){
@@ -106,7 +92,7 @@ var createCSSChangeArray = function(){
 
 //Changes current month text 
 var changeMonthText = function(){
-	currentMonth = months[currentIndex];
+	currentMonth = months[currentMonthIndex];
 	document.getElementById("current").innerHTML = 
 	"&nbsp;&nbsp;"
 	+ currentMonth
@@ -117,16 +103,41 @@ var changeMonthText = function(){
 //Populates array of dates
 var populateDays = function(){
 	for (var i=0; i < between.length; i++){
-		DateList.push(between[i].getDate());
+		dateList.push(between[i].getDate());
 	}
-	DateList.reverse();
-	DateList.shift();		
+	dateList.reverse();
+	dateList.shift();		
 	this.dates.removeAll();
 	for (var i=0; i < 49; i++){
-		this.dates.push(DateList[i]);
+		this.dates.push(dateList[i]);
 	}
 }
 
+//Converts raw dates to date indexes
+var cssSelection = function(changeThis){
+	dateSelection = dateList.find(findDate);				
+	function findDate(element) {
+		return element == changeThis;
+	}
+	if (dateSelection > 18){
+		cssEvents((dateList.lastIndexOf(dateSelection))+1);
+	}
+	if (dateSelection <= 18){
+		cssEvents((dateList.indexOf(dateSelection))+1);
+	} 
+}
+
+//Iterates over date array for conversion into cssSelection
+var dateArrayConverter = function(){
+	selectIndex = 0;
+	for (var i=0; i < userIndexes().length; i++){
+		if ((userIndexes()[i]) == currentTimeIndex){
+			cssSelection(userDateArray()[i]);
+		}
+	}
+}
+
+//Converts observables to numbers instead of strings
 ko.extenders.number = function(observable, opt) {
    return ko.computed({
        read: observable,
@@ -142,64 +153,22 @@ ko.extenders.number = function(observable, opt) {
    });
 }
 
-
-//CONVERTS RAW DATES TO DATE INDEXES
-var cssSelection = function(changeThis){
-	dateSelection = DateList.find(findDate);
-				
-		function findDate(element) {
-			return element == changeThis;
-		}
-		
-		if (dateSelection > 18){
-			cssChanger3((DateList.lastIndexOf(dateSelection))+1);
-		}
-		
-		if (dateSelection <= 18){
-			cssChanger3((DateList.indexOf(dateSelection))+1);
-		} 
-}
-
-//CONVERTS RAW DATES TO DATE INDEXES
-var cssSelection2 = function(changeThis){
-	dateSelection = DateList.find(findDate);
-				
-		function findDate(element) {
-			return element == changeThis;
-		}
-		
-		if (dateSelection > 18){
-			cssChanger4((DateList.lastIndexOf(dateSelection))+1);
-		}
-		
-		if (dateSelection <= 18){
-			cssChanger4((DateList.indexOf(dateSelection))+1);
-		} 
-}
-
-
-
 //-------------------------------------------------------------------------------VIEWMODEL
 
 var ViewModel = function() {
 
 	//---------------------------------------------------------------------------VIEWMODEL FUNCTIONS
 	
-	
 	this.userYear = ko.observable().extend({ number: true });
 	this.userMonth = ko.observable().extend({ number: true });
 	this.userDate = ko.observable().extend({ number: true });
 	this.userColor = ko.observable();
 	
-
-
-
-	
 	//Array of visible user converted dates
-	this.userTest = ko.observableArray();
+	this.eventList = ko.observableArray();
 	
 	//Array of user date numbers
-	this.userTestDates = ko.observableArray();
+	this.userDateArray = ko.observableArray();
 	
 	//Array of user month indexes
 	this.userIndexes = ko.observableArray();
@@ -207,21 +176,24 @@ var ViewModel = function() {
 	//Array of user colors
 	this.userColorArray = ko.observableArray();
 	
+	//Year index (January 2017 = 24204)
 	this.yearIndex = ko.computed(function() {
         return ((((this.userYear()*12))));
     }, this);
 	
-	this.MonthIndex = ko.computed(function() {
+	//Month index (0-11)
+	this.monthIndex = ko.computed(function() {
         return ((((this.userMonth()))));
     }, this);
 	
+	//Year index & month index combined
 	this.userTimeIndex = ko.computed(function() {
-        return ((this.yearIndex())+(this.MonthIndex()));
+        return ((this.yearIndex())+(this.monthIndex()));
     }, this);
 
-	
 	this.prevDates = ko.observableArray();	
 	
+	//Loads user selected date for view or event add
 	this.loadUserDate = function(){
 		this.userTimeChange = ko.computed(function() {
 			return (currentTimeIndex-(this.userTimeIndex()));
@@ -240,100 +212,97 @@ var ViewModel = function() {
 			}
 		}
 		
-		//CONVERTS RAW DATES TO DATE INDEXES
-		dateSelection = DateList.find(findDate);	
-			
+		//Converts raw dates to date indexes
+		dateSelection = dateList.find(findDate);	
 		function findDate(element) {
 			return element == userDate();
 		}
-		
 		if (prevDates().length > 0){
-			cssChanger5(prevDates.pop());
+			cssRemove(prevDates.pop());
 		}
-		
 		if (dateSelection > 18){
-			cssChanger2((DateList.lastIndexOf(dateSelection))+1);
-			prevDates.push((DateList.lastIndexOf(dateSelection))+1);
+			cssSelectedDate((dateList.lastIndexOf(dateSelection))+1);
+			prevDates.push((dateList.lastIndexOf(dateSelection))+1);
 		}
-		
 		if (dateSelection <= 18){
-			cssChanger2((DateList.indexOf(dateSelection))+1);
-			prevDates.push((DateList.indexOf(dateSelection))+1);
+			cssSelectedDate((dateList.indexOf(dateSelection))+1);
+			prevDates.push((dateList.indexOf(dateSelection))+1);
 		}
 	}
 
-	
-	//EVENT BUTTON ON CLICK
+	//Add Event button on click
 	this.addEvent = function(){
 		
-		
+		//User date input converted to string for event list
 		var search = ' ' + (userMonth()+1).toString() + '/' + userDate().toString() + '/' + userYear().toString();
-		var occurances = userTest().filter(function(val){
+		
+		//Number of times user event added on same day
+		var eventOccurences = eventList().filter(function(val){
 			return val === search;
 		}).length;
 	
-		if (occurances < 2){
+		if (eventOccurences < 2){
 		
-		userTest.push(' ' + (userMonth()+1).toString() + '/' + userDate().toString() + '/' + userYear().toString());
-		userTestDates.push(userDate());
-		userColorArray.push(userColor());
-		userIndexes.push(userTimeIndex());
+			//Adds user selected date to associated arrays
+			eventList.push(' ' + (userMonth()+1).toString() + '/' + userDate().toString() + '/' + userYear().toString());
+			userDateArray.push(userDate());
+			userColorArray.push(userColor());
+			userIndexes.push(userTimeIndex());
+			
+			//If user selected date is in current month, display
+			if (userTimeIndex() == currentTimeIndex){
+				cssSelection(userDate());	
+			}
+			colorChanger();
 		
-		if (userTimeIndex() == currentTimeIndex){
-			cssSelection(userDate());	
+			//Changes event list item color
+			var ul = document.getElementById("eventList");
+			var items = ul.getElementsByTagName("li");
+			for (var i = 0; i < items.length; i++) {
+				items[i].style.color = userColorArray()[i];
+			}
 		}
-
-		colorChanger();
 	
-	var ul = document.getElementById("userTest");
-	var items = ul.getElementsByTagName("li");
-	for (var i = 0; i < items.length; i++) {
-		items[i].style.color = userColorArray()[i];
-	}
-	}
-	
-	if (occurances == 2){
-		alert('Can not add more than two events on the same day.');
-	}
+		if (eventOccurences == 2){
+			alert('Can not add more than two events on the same day.');
+		}
 	
 	}
 	
+	//Remove Event button on click
 	this.removeEvent = function(){
-		userTest.remove(' ' + (userMonth()+1).toString() + '/' + userDate().toString() + '/' + userYear().toString());
-		userTestDates.splice((userIndexes().indexOf(userTimeIndex())),1);
+		
+		//Removes user selected date from associated arrays
+		eventList.remove(' ' + (userMonth()+1).toString() + '/' + userDate().toString() + '/' + userYear().toString());
+		userDateArray.splice((userIndexes().indexOf(userTimeIndex())),1);
 		userColorArray.splice((userIndexes().indexOf(userTimeIndex())),1);
 		userIndexes.remove(userTimeIndex());
 
-	selectIndex = 0;
-		for (var i=0; i < userIndexes().length; i++){
-			if ((userIndexes()[i]) == currentTimeIndex){
-				cssSelection(userTestDates()[i]);
-			}
+		dateArrayConverter();
+		 
+		if (userTimeIndex() == currentTimeIndex){
+			cssSelection(userDate());	
 		}
-		colorChanger();
-		
+		colorChanger();	
 	}
 	
-	
-	
-
 	//Changes current date array
 	var setNewDate = function(){
-		date1 = new Date(2017,2,(startingDate1));
-		date2 = new Date(2017,0,(startingDate2));
-		between = [date1];
+		monthEnd = new Date(2017,2,(startingDate1));
+		monthStart = new Date(2017,0,(startingDate2));
+		between = [monthEnd];
 
-		while(date2 <= date1) {
-			day = date1.getDate()
-			date1 = new Date(date1.setDate(--day));  
-			between.push(date1);
+		while(monthStart <= monthEnd) {
+			var day = monthEnd.getDate()
+			monthEnd = new Date(monthEnd.setDate(--day));  
+			between.push(monthEnd);
 		}
 	}
 	
-	//Applies cssChanger function to pre/postpend array
+	//Applies cssNotCurrentMonth function to pre/postpend array
 	var applyCSSToPrePost = function(){	
 		for (var i=0; i < pre_post_Dates.length; i++){
-			cssChanger(pre_post_Dates[i]);
+			cssNotCurrentMonth(pre_post_Dates[i]);
 		}
 	}		
 
@@ -347,15 +316,15 @@ var ViewModel = function() {
 	
 	setNewDate();
 
-	DateList = [];
+	dateList = [];
 
 	//Sets initial calendar array
 	for (var i=0; i < between.length; i++){
-		DateList.push(between[i].getDate());
+		dateList.push(between[i].getDate());
 	}
 	
-	DateList.reverse();
-	DateList.shift();
+	dateList.reverse();
+	dateList.shift();
 
 	createCSSChangeArray();
 	
@@ -363,10 +332,8 @@ var ViewModel = function() {
 	this.daysoftheweek = ko.observableArray(['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']);
 	
 	//Bottom 7 rows of calendar
-	this.dates = ko.observableArray(DateList);
+	this.dates = ko.observableArray(dateList);
 	
-	var periods = ".";
-		
 	//-------------------------------------------------------------------------GO FORWARD IN TIME
 	
 	//Shifts days forward on click
@@ -376,10 +343,10 @@ var ViewModel = function() {
 		
 		//Increment values
 		//After December, loop back to January
-		currentIndex+=1;
+		currentMonthIndex+=1;
 		currentTimeIndex +=1;
-		if (currentIndex == 12){
-			currentIndex = 0;
+		if (currentMonthIndex == 12){
+			currentMonthIndex = 0;
 			currentYear += 1;
 		}
 
@@ -406,7 +373,7 @@ var ViewModel = function() {
 
 		//---------------------------------------------------------------------POPULATE NEXT MONTH
 		
-		DateList = [];
+		dateList = [];
 		populateDays();
 		
 		//-----------------------------------------------------------------APPLY STYLE TO POST/PRE DATES
@@ -414,13 +381,8 @@ var ViewModel = function() {
 		pre_post_Dates = [];
 		createCSSChangeArray();	
 		applyCSSToPrePost();
-		
-		selectIndex = 0;
-		for (var i=0; i < userIndexes().length; i++){
-			if ((userIndexes()[i]) == currentTimeIndex){
-				cssSelection(userTestDates()[i]);
-			}
-		}
+				
+		dateArrayConverter();
 		colorChanger();
 	};
 		
@@ -433,10 +395,10 @@ var ViewModel = function() {
 		
 		//Decrement values
 		//After January, loop back to December
-		currentIndex-=1;
+		currentMonthIndex-=1;
 		currentTimeIndex -=1;
-		if (currentIndex == -1){
-			currentIndex = 11;
+		if (currentMonthIndex == -1){
+			currentMonthIndex = 11;
 			currentYear -= 1;
 		}
 	
@@ -463,7 +425,7 @@ var ViewModel = function() {
 	
 		//----------------------------------------------------------------POPULATE PREVIOUS MONTH
 
-		DateList = [];
+		dateList = [];
 		populateDays();
 		
 		//----------------------------------------------------------------APPLY STYLE TO POST/PRE DATES
@@ -471,13 +433,8 @@ var ViewModel = function() {
 		pre_post_Dates = [];
 		createCSSChangeArray();	
 		applyCSSToPrePost();
-		
-		selectIndex = 0;
-		for (var i=0; i < userIndexes().length; i++){
-			if ((userIndexes()[i]) == currentTimeIndex){
-				cssSelection(userTestDates()[i]);
-			}
-		}
+
+		dateArrayConverter();
 		colorChanger();
 	};	
 };
